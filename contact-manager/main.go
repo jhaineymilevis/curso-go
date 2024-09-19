72413374b9b4dbd949781fa5f2b8f0b1ac9468c7
Jhainey Perez <jhainey.perez@dropi.co>
2024-09-19 09:49:38 -0500

    creado un gestor de contactos

--- contact-manager/main.go
+++ contact-manager/main.go
@@ -1,30 +1,100 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 )
 
-func main() {
-	//defer
-	defer fmt.Println(3) //esta funcion se ejecuta al final
-	fmt.Println(2)
-	fmt.Println(1)
+type Contact struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
 
-	//crear archivo
-	file, err := os.Create("hola.txt")
+func saveContactToFile(contacts []Contact) error {
+	file, err := os.Create("contacts.json")
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
-	//cerrar el archivo con defer para que aunque falle lo demas, siempre se ejecute al final
 	defer file.Close()
-
-	//escribir en el archivo
-	_, err = file.Write([]byte("hola mundo"))
+	econder := json.NewEncoder(file)
+	err = econder.Encode(contacts)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
+	}
+
+	return nil
+}
+
+func loadContactsFromFile(contacts *[]Contact) error {
+	file, err := os.Open("contacts.json")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&contacts)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func main() {
+
+	var contacts []Contact
+	err := loadContactsFromFile(&contacts)
+	if err != nil {
+		fmt.Printf("Error loading contacts")
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("====== GESTOR DE CONTACTOS ====== \n",
+			"1. Agregar contacto \n",
+			"2. Mostrar contactos \n",
+			"3. Salir \n",
+			"Seleccione una opcion: ")
+
+		var option int
+		_, err = fmt.Scanln(&option)
+		if err != nil {
+			fmt.Println("Error al leer la opcion")
+			return
+		}
+
+		switch option {
+		case 1:
+			var c Contact
+			fmt.Print("Ingrese el nombre: ")
+			c.Name, _ = reader.ReadString('\n')
+			fmt.Print("Ingrese el email: ")
+			c.Email, _ = reader.ReadString('\n')
+			fmt.Print("Ingrese el telefono: ")
+			c.Phone, _ = reader.ReadString('\n')
+			contacts = append(contacts, c)
+			err = saveContactToFile(contacts)
+			if err != nil {
+				fmt.Println("Error al guardar los contactos")
+			}
+
+		case 2:
+			fmt.Println("================================================")
+			for index, contact := range contacts {
+				fmt.Printf("%d. Nombre: %s - Email: %s - Telefono: %s\n", index+1, contact.Name, contact.Email, contact.Phone)
+			}
+			fmt.Println("================================================")
+
+		case 3:
+			return
+		default:
+			fmt.Println("Opcion invalida")
+
+		}
 	}
 
 }
--- errores/main.go
+++ errores/main.go
@@ -1,1 +1,81 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func divide(dividendo, divisor int) (int, error) {
+	//funcion anonima
+	defer func() { // con defer se ejecuta al final de la cuncion divide
+		//implementamos recover, con esto se logra que el panic no interrumpa nuestra aplicacion
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+	//con trol de erroes
+	/*if divisor == 0 {
+		return 0, errors.New("No se puede dividir por cero")
+	}*/
+	validateZero(divisor) //control de erroes con panic
+	return dividendo / divisor, nil
+
+}
+
+func validateZero(divisor int) {
+
+	if divisor == 0 {
+		panic("No se puede dividir por cero") //panic
+	}
+}
+
+func main() {
+
+	//errores
+	result, err := divide(10, 2)
+	// paninc y recover . no se recomienda para el flujo normal de control del programa. se recomienda para casos excepcionales o errores graves o situaciones inesperdas. manejar limpieza antyes de finalizar el programa
+	divide(10, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("resultados:", result)
+
+	//defer
+	defer fmt.Println(3) //esta funcion se ejecuta al final
+	fmt.Println(2)
+	fmt.Println(1)
+
+	//crear archivo
+	file, err := os.Create("hola.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//cerrar el archivo con defer para que aunque falle lo demas, siempre se ejecute al final
+	defer file.Close()
+
+	//escribir en el archivo
+	_, err = file.Write([]byte("hola mundo"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//regstro de erores
+	log.SetPrefix("main(): ") // agrega un prefijo
+	log.Println("ejecutando progrma")
+
+	//regiustrar los erroes en un archiuvo
+	file2, err2 := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	defer file2.Close()
+	log.SetOutput(file2)
+	log.Print("oye, soy un log")
+
+	//log.Fatal("error en la aplicacion fatal, esto detiene el programa")
+	//log.Panic("error en la aplicacion panico, esto detiene el programa")
+}
