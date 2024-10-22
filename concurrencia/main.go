23dd69ce3f664b357bb313592342eae8b8876cd1
Jhainey Perez <jhainey.perez@dropi.co>
2024-10-22 16:20:17 -0500

    managin chanels for go runtimes comunication

--- concurrencia/main.go
+++ concurrencia/main.go
@@ -22,25 +22,38 @@ func main() {
 		"https://test.noexiste.com",
 	}
 
+	//declaring a chanel for comunicate go runtimes
+	channel := make(chan string)
+
 	for _, api := range apis {
 		// go runtimes
-		go checkApi(api)
+		go checkApi(api, channel)
+	}
+
+	for i := 0; i < len(apis); i++ {
+		fmt.Print(<-channel) //print channel data
 	}
 
 	//suspence time to see the result
-	time.Sleep(15 * time.Second)
+	//time.Sleep(15 * time.Second)
 
 	elapsed := time.Since(start)
 	fmt.Printf("Finished on : %v\n seconds", elapsed.Seconds())
+
 }
 
-func checkApi(api string) {
+func checkApi(api string, channel chan string) {
+
 	_, err := http.Get(api)
 
 	if err != nil {
-		fmt.Printf("Error Api Down %s: %v\n", api, err)
+		//fmt.Printf("Error Api Down %s: %v\n", api, err)             // prit bny console
+		channel <- fmt.Sprintf("ERROR! Api Down %s: %v\n", api, err) //send data to chanel
+
 		return
 	}
 
-	fmt.Printf("Api esta en funcionamiento %s: %v\n", api, err)
+	//fmt.Printf("Api esta en funcionamiento %s: %v\n", api, err)             // prit bny console
+	channel <- fmt.Sprintf("SUCCESS: Api esta en funcionamiento %s: %v\n", api, err) //send data to chanel
+
 }
