4275bc62884c5022b44314f7b6d0d59a65cff8d1
Jhainey Perez <jhainey.perez@dropi.co>
2024-12-18 06:45:06 -0500

    creating albums handler

--- vinil-api/main.go
+++ vinil-api/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "BLUE RAIN", Artist: "GUNS AND ROSES", Price: 12.9},
+	{ID: "2", Title: "19", Artist: "ABBA", Price: 10.9},
+	{ID: "3", Title: "REVOLUTION", Artist: "REM", Price: 14.9},
+}
+
+func getAlbums(ctx *gin.Context) {
+	ctx.IndentedJSON(
+		http.StatusOK,
+		albums,
+	)
+}
 func main() {
 	router := gin.Default()
 
 	router.GET(
-		"/",
-		func(ctx *gin.Context) {
-			ctx.JSON(
-				200,
-				gin.H{
-					"message": "Hello, World!",
-				},
-			)
-		},
+		"/albums",
+		getAlbums,
 	)
 
 	router.Run("localhost:8080")
