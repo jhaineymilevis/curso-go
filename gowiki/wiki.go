33722a9501c1b2f8176af38bc5e29b900cd2d869
Jhainey Perez <jhainey.perez@dropi.co>
2025-06-13 15:31:08 -0500

    Refactor main function to include HTTP handler for dynamic responses

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -27,15 +28,25 @@ func loadPage(title string) (*Page, error) {
 
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path[1:] // Elimina la barra inicial
+	fmt.Fprintf(w, "Hola! me encantan los %s", message)
+}
+
 func main() {
-	p1 := &Page{Title: "Test page", Body: []byte("This is a test page")}
-	p1.save()
+	// p1 := &Page{Title: "Test page", Body: []byte("This is a test page")}
+	// p1.save()
 
-	p2, err := loadPage("Test page")
+	// p2, err := loadPage("Test page")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(string(p2.Body))
+	http.HandleFunc("/", handler)
+
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
-	fmt.Println(string(p2.Body))
-
 }
