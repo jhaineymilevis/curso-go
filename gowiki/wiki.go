eee5f26a6af0f0ba8e561f9e7e1ac9a6283eb6d9
Jhainey Perez <jhainey.perez@dropi.co>
2025-06-13 15:48:25 -0500

    Add view and edit handlers with corresponding HTML templates for page management

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"os"
 )
@@ -33,6 +34,39 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hola! me encantan los %s", message)
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		http.Error(w, "Page not found", http.StatusNotFound)
+		return
+	}
+
+	renderRemplates(w, "view.html", p)
+}
+
+func editPageHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		http.Error(w, "Page not found", http.StatusNotFound)
+		return
+	}
+
+	renderRemplates(w, "edit.html", p)
+
+}
+
+func savePageHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func renderRemplates(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl)
+	t.Execute(w, p)
+
+}
+
 func main() {
 	// p1 := &Page{Title: "Test page", Body: []byte("This is a test page")}
 	// p1.save()
@@ -44,6 +78,9 @@ func main() {
 	// }
 	// fmt.Println(string(p2.Body))
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editPageHandler)
+	http.HandleFunc("/save/", editPageHandler)
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
