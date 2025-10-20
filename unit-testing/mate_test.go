b9558031a6b1b1ca024ca1ed92db9ad683e26134
Jhainey Perez <jhainey.perez@dropi.co>
2025-10-20 11:45:06 -0500

    Refactor TestSumar to use table-driven tests and add coverage report

--- unit-testing/mate_test.go
+++ unit-testing/mate_test.go
@@ -4,10 +4,29 @@ import "testing"
 
 func TestSumar(t *testing.T) {
 
-	result := Sumar(2, 3)
+	/*result := Sumar(2, 3)
 	expected := 5
 
 	if result != expected {
 		t.Errorf("Error al sumar: se esperaba %d, pero se obtuvo %d", expected, result)
+	}*/
+
+	casos := []struct {
+		a, b     int
+		expected int
+	}{
+		{2, 3, 5},
+		{0, 0, 0},
+		{-1, 1, 0},
+		{-2, -3, -5},
+		{100, 200, 300},
 	}
+
+	for _, caso := range casos {
+		result := Sumar(caso.a, caso.b)
+		if result != caso.expected {
+			t.Errorf("Error al sumar %d y %d: se esperaba %d, pero se obtuvo %d", caso.a, caso.b, caso.expected, result)
+		}
+	}
+
 }
