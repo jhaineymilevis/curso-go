88021921c0f1a40b8dbd58dba279a6d84b97f887
Jhainey Perez <mielvisj@gmail.com>
2026-01-20 14:27:57 -0500

    Refactor template rendering in handlers to use a common renderTemplate function and update index.html to utilize base template structure

--- piedra-papel-o-tijera/handlers/handlers.go
+++ piedra-papel-o-tijera/handlers/handlers.go
@@ -2,61 +2,41 @@ package handlers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
+const (
+	templateDir  = "templates/"
+	templateBase = templateDir + "base.html"
+)
+
 func Index(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		http.Error(w, "Error al cargar la plantilla", http.StatusInternalServerError)
-		return
-	}
 
-	data := struct {
-		Title   string
-		Message string
-	}{
-		Title:   "Piedra, Papel o Tijera",
-		Message: "¡Bienvenido al juego!",
-	}
-
-	if err := tmpl.Execute(w, data); err != nil {
-		http.Error(w, "Error al ejecutar la plantilla", http.StatusInternalServerError)
-		return
-	}
+	renderTemplate(w, "index.html", nil)
 }
 
 func NewGame(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/new_game.html")
-	if err != nil {
-		http.Error(w, "Error al cargar la plantilla", http.StatusInternalServerError)
-		return
-	}
-	tmpl.Execute(w, nil)
+	renderTemplate(w, "new_game.html", nil)
 }
 func Game(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/game.html")
-	if err != nil {
-		http.Error(w, "Error al cargar la plantilla", http.StatusInternalServerError)
-		return
-	}
-	tmpl.Execute(w, nil)
+	renderTemplate(w, "game.html", nil)
 }
 
 func Play(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/play.html")
-	if err != nil {
-		http.Error(w, "Error al cargar la plantilla", http.StatusInternalServerError)
-		return
-	}
-	tmpl.Execute(w, nil)
+	renderTemplate(w, "play.html", nil)
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/about.html")
-	if err != nil {
-		http.Error(w, "Error al cargar la plantilla", http.StatusInternalServerError)
+	renderTemplate(w, "about.html", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, page string, data any) {
+	tmpl := template.Must(template.ParseFiles(templateBase, templateDir+page))
+
+	if err := tmpl.ExecuteTemplate(w, "base", data); err != nil {
+		http.Error(w, "Error al ejecutar la plantilla", http.StatusInternalServerError)
+		log.Println("Error al ejecutar la plantilla:", err)
 		return
 	}
-	tmpl.Execute(w, nil)
 }
